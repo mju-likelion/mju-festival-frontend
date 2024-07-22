7489239ef22e99c2fa2665fbd4fbce70c2cbe0ce
hyunyeee <5004ohy@naver.com>
2024-07-23 04:23:47 +0900

    setting: react-router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { theme } from './styles';
 import GlobalStyle from './styles/GlobalStyle';
+import RouteChangeTracker from './components/RouteChangeTracker.tsx';
+import Main from './pages/Main.tsx';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter />
+      <RouteChangeTracker />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
