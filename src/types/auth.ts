280edbbcc3c3173f1b55cb62b2e50b4259993eee
hyunyeee <5004ohy@naver.com>
2024-07-26 18:55:44 +0900

    feat: encrypt 정보 response type 추가 -  #1

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,3 +2,21 @@ export interface AuthFormValues {
   id: string;
   password: string;
 }
+
+export interface LogInFormDataValues {
+  studentId: string;
+  password: string;
+  decryptionMethod: 'TOKEN' | 'KEY';
+  decryptionValue: string;
+}
+
+export interface EncryptKeyInformaion {
+  message: string;
+  data: EncryptKey;
+}
+
+export interface EncryptKey {
+  rsaPublicKey: string;
+  keyStorageStrategy: 'TOKEN' | 'KEY';
+  credential: string;
+}
