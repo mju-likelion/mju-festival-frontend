1f27b8b5cbeaf63d8eec20c3fe77b01feb51cf93
zelkovaria <hyein396@gmail.com>
2024-08-17 15:35:34 +0900

    fix: 조건 수정 - #13

--- src/utils/downloadAppUtils.ts
+++ src/utils/downloadAppUtils.ts
@@ -4,10 +4,13 @@ export const downloadAppByDevice = () => {
     mobileType.includes('mac') ||
     mobileType.includes('iphone') ||
     mobileType.includes('ipad')
-  )
+  ) {
     window.location.href = import.meta.env.VITE_IOS_DOWNLOAD_URL;
+    return;
+  }
   if (mobileType.includes('android')) {
     window.location.href = import.meta.env.VITE_ANDROID_DOWNLOAD_URL;
+    return;
   }
   alert("해당 기기의 마켓에서 '티켓프렌즈'를 검색해주세요");
 };
