3eced091865968e5ed5215b332faf8c00a0b76be
hyunyeee <5004ohy@naver.com>
2024-08-20 23:44:08 +0900

    feat: 부스 단건조회 페이지 역할에 따른 뷰 구현 - #23

--- src/pages/Booth/BoothDetail.tsx
+++ src/pages/Booth/BoothDetail.tsx
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 import { useCallback, useEffect, useState } from 'react';
 import { getBoothDetail } from '../../api/booth.ts';
 import { BoothDetailInfo } from '../../types';
+import { useAuthStore } from '../../store';
 
 const BoothDetail = () => {
+  const { role } = useAuthStore();
   const [boothDetailData, setBoothDetailData] = useState<BoothDetailInfo>({
     createdAt: '',
     description: '',
@@ -35,6 +37,7 @@ const BoothDetail = () => {
       <button type="button" onClick={() => navigate('/booths')}>
         뒤로가기
       </button>
+      <h1>{role}</h1>
       <Box>
         <p>id: {id}</p>
         <p>부스 이름: {name}</p>
@@ -43,6 +46,15 @@ const BoothDetail = () => {
         <p>생성 시간: {createdAt}</p>
         <Img src={imageUrl} alt="부스 이미지" />
       </Box>
+      {role === 'STUDENT' && (
+        <StudentAction>
+          <Buttons>
+            <Button>QR 촬영하기</Button>
+            <Button>도장판으로</Button>
+          </Buttons>
+        </StudentAction>
+      )}
+      {role === 'BOOTH_MANAGER' && <BoothManagerAction>QR</BoothManagerAction>}
     </Wrapper>
   );
 };
@@ -51,9 +63,21 @@ const Wrapper = styled.div`
   border: 1px solid dodgerblue;
 `;
 const Box = styled.div`
-  border: 1px solid pink;
+  border: 3px solid #ff8f4c;
 `;
 const Img = styled.img`
   width: 300px;
 `;
+const StudentAction = styled.div`
+  border: 3px solid pink;
+`;
+const BoothManagerAction = styled.div`
+  border: 3px solid lightgreen;
+`;
+const Buttons = styled.button`
+  display: flex;
+  gap: 20px;
+`;
+const Button = styled.button``;
+
 export default BoothDetail;
