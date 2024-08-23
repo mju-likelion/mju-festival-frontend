5790e193931af5545f9780d1212e203a7f9d3c4d
constantly-dev <rntqhdl10@naver.com>
2024-08-23 15:07:10 +0900

    setting: qrcode.react 추가 - #8

--- src/components/QrBottomSheet/Base.tsx
+++ src/components/QrBottomSheet/Base.tsx
@@ -3,20 +3,21 @@ import BottomSheet from '.';
 
 const Base = () => {
   return (
-    <BaseDiv>
+    <>
+      <BaseDiv />
       <BottomSheet />
-    </BaseDiv>
+    </>
   );
 };
 
 const BaseDiv = styled.div`
   width: 100%;
+  /* height: calc(var(--vh, 1vh) * 100); */
   height: 100vh;
   background-color: skyblue;
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 2px solid blue;
 `;
 
 export default Base;
