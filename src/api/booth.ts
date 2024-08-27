132f81b5dee9db2654ff1f509bd87d9859d87157
hyunyeee <5004ohy@naver.com>
2024-08-27 15:42:11 +0900

    refactor: 구조 분해 할당 사용으로 불필요한 변수 생략 - #23

--- src/api/booth.ts
+++ src/api/booth.ts
@@ -9,22 +9,22 @@ import {
 } from '../types';
 
 export const getBoothList = async () => {
-  const response = await Axios.get<BoothList>(`/booths?page=${0}&size=${30}`);
-  return response.data.simpleBooths;
+  const { data } = await Axios.get<BoothList>(`/booths?page=${0}&size=${30}`);
+  return data.simpleBooths;
 };
 
 export const getBoothDetail = async (boothId: string) => {
-  const response = await Axios.get<BoothDetailInfo>(`/booths/${boothId}`);
-  return response.data;
+  const { data } = await Axios.get<BoothDetailInfo>(`/booths/${boothId}`);
+  return data;
 };
 
 export const getOwnership = async (token: string, boothId: string) => {
-  const response = await Axios.get<Ownership>(`/booths/${boothId}/ownership`, {
+  const { data } = await Axios.get<Ownership>(`/booths/${boothId}/ownership`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  return response.data.isOwner;
+  return data.isOwner;
 };
 
 export const patchBoothDetail = async (
@@ -32,12 +32,12 @@ export const patchBoothDetail = async (
   updateData: Partial<BoothEditFields>,
   token: string
 ) => {
-  const response = await Axios.patch(`/booths/${boothId}`, updateData, {
+  const { data } = await Axios.patch(`/booths/${boothId}`, updateData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  return response.data;
+  return data;
 };
 
 export const getQrData = async (token: string, id: string) => {
