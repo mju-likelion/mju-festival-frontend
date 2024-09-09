0c1f09175c0ffd6bee37e4eaec5b45915f474de4
constantly-dev <rntqhdl10@naver.com>
2024-09-09 14:38:37 +0900

    feat: 분실물 삭제 api 연동 - #43

--- src/pages/ViewDetailLostItem/index.tsx
+++ src/pages/ViewDetailLostItem/index.tsx
@@ -1,10 +1,30 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { deleteLostItem } from '../../api/lostItem';
+import { useAuthStore } from '../../store';
 import Header from './Header';
 
 const DetailLostItem = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { token, role } = useAuthStore();
   const { title, content, createdAt, imageUrl, isFounded } = location.state;
+
+  const handleDelete = async () => {
+    if (window.confirm('정말로 이 아이템을 삭제하시겠습니까?')) {
+      try {
+        if (id && token) {
+          await deleteLostItem(id, token);
+          navigate('/lost-items');
+        } else {
+          console.error('삭제를 위한 ID나 토큰이 없습니다.');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <Wrapper>
@@ -16,6 +36,9 @@ const DetailLostItem = () => {
         <ItemContent defaultValue={content} />
       </ItemLayout>
       {isFounded ? <FoundP>찾았다!!!!</FoundP> : null}
+      {role === 'STUDENT_COUNCIL' && (
+        <DeleteButton onClick={handleDelete}>삭제하기</DeleteButton>
+      )}
     </Wrapper>
   );
 };
@@ -42,4 +65,6 @@ const ItemContent = styled.textarea`
 const FoundP = styled.p`
   color: blue;
 `;
+
+const DeleteButton = styled.button``;
 export default DetailLostItem;
