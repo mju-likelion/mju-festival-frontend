4f4f7c6844c1671ad20c76063cd7694590716965
hyunyeee <5004ohy@naver.com>
2024-09-15 14:23:42 +0900

    design: 학생, 관리자 로그인 페이지 스타일링 - #45

--- src/pages/LogIn/LogInButton.tsx
+++ src/pages/LogIn/LogInButton.tsx
@@ -1,5 +1,16 @@
+import styled from 'styled-components';
+
 const LogInButton = () => {
-  return <button type="submit">로그인</button>;
+  return <Button type="submit">로그인</Button>;
 };
 
+const Button = styled.button`
+  width: 100%;
+  margin-top: 32px;
+  padding: 16px 0;
+  border-radius: 12px;
+  ${({ theme }) => theme.typographies.body1};
+  color: ${({ theme }) => theme.colors.white100};
+  background-color: ${({ theme }) => theme.colors.blue100};
+`;
 export default LogInButton;
--- src/pages/LogIn/indexAdmin.tsx
+++ src/pages/LogIn/indexAdmin.tsx
@@ -1,21 +1,61 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import LogInForm from './LogInForm.tsx';
+import { ReactComponent as Logo } from '../../assets/imgs/logo.svg';
+import { ReactComponent as SubTitleLogo } from '../../assets/imgs/logo_subtitle.svg';
+import { ReactComponent as BackIcon } from '../../assets/icons/left_arrow.svg';
 
 const AdminLogIn = () => {
   const navigate = useNavigate();
   return (
-    <Wrapper>
-      <button type="button" onClick={() => navigate('/login')}>
+    <>
+      <BackButton role="button" onClick={() => navigate('/login')}>
+        <BackIcon />
         뒤로가기
-      </button>
-      <LogInForm setIsModalOpen={() => {}} />
-    </Wrapper>
+      </BackButton>
+      <Wrapper>
+        <Header>
+          <LogoWrapper>
+            <Logo width={126} />
+            <SubTitleLogo />
+          </LogoWrapper>
+          <Role>관리자용</Role>
+        </Header>
+        <LogIn>LOGIN</LogIn>
+        <LogInForm setIsModalOpen={() => {}} />
+      </Wrapper>
+    </>
   );
 };
 
 const Wrapper = styled.div`
-  border: 1px solid black;
+  padding: 0 20px;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Role = styled.p`
+  color: ${({ theme }) => theme.colors.text500};
+  ${({ theme }) => theme.typographies.subhead2};
+`;
+const BackButton = styled.div`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.text500};
+  ${({ theme }) => theme.typographies.footnote};
+`;
+const LogoWrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+const LogIn = styled.p`
+  margin: 80px 0 32px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.blue100};
+  ${({ theme }) => theme.typographies.largeTitle};
 `;
 
 export default AdminLogIn;
