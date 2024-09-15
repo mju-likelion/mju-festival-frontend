6a82fd57ba0a7202536e6b9d821d3af9497b0251
hyunyeee <5004ohy@naver.com>
2024-09-15 14:22:13 +0900

    design: 비밀번호 확인 버튼 디자인 - #45

--- src/pages/LogIn/LogInInput.tsx
+++ src/pages/LogIn/LogInInput.tsx
@@ -1,4 +1,7 @@
+import styled from 'styled-components';
 import { FieldPath, FieldValues, UseFormRegister } from 'react-hook-form';
+import { ReactComponent as EyeClose } from '../../assets/icons/pwd_close.svg';
+import { ReactComponent as EyeOpen } from '../../assets/icons/pwd_open.svg';
 
 interface InputPropTypes<T extends FieldValues> {
   type: 'text' | 'password';
@@ -7,6 +10,8 @@ interface InputPropTypes<T extends FieldValues> {
   register: UseFormRegister<T>;
   // eslint-disable-next-line react/require-default-props
   toggleEye?: () => void;
+  // eslint-disable-next-line react/require-default-props
+  isOpen?: boolean;
 }
 
 const LogInInput = <T extends FieldValues>({
@@ -15,21 +20,44 @@ const LogInInput = <T extends FieldValues>({
   placeholder,
   register,
   toggleEye = () => {},
+  isOpen,
 }: InputPropTypes<T>) => {
   return (
-    <div>
-      <input
+    <Box>
+      <Input
         type={type}
         placeholder={placeholder}
         {...register(name as FieldPath<T>)}
       />
       {name === 'password' && (
-        <button type="button" onClick={toggleEye}>
-          눈
-        </button>
+        <Button
+          type="button"
+          onClick={toggleEye}
+          aria-label="비밀번호 입력 값 숨기기"
+        >
+          {isOpen ? <EyeOpen /> : <EyeClose />}
+        </Button>
       )}
-    </div>
+    </Box>
   );
 };
 
+const Box = styled.div`
+  width: 100%;
+  padding: 16px 12px;
+  display: flex;
+  background-color: ${({ theme }) => theme.colors.black10};
+  border-radius: 12px;
+  input {
+    ${({ theme }) => theme.typographies.body2};
+  }
+`;
+const Input = styled.input`
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.text500};
+  }
+`;
+const Button = styled.button`
+  height: 20px;
+`;
 export default LogInInput;
