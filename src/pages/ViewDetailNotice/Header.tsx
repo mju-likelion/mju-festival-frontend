f099619cfb977a3486be207c56813d80db1bfa35
zelkovaria <hyein396@gmail.com>
2024-09-16 19:07:00 +0900

    feat: 헤더의 뒤로가기 버튼 삭제 및 onClick 영역 수정 - #47

--- src/pages/ViewDetailNotice/Header.tsx
+++ src/pages/ViewDetailNotice/Header.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuthStore } from '../../store';
+import { ReactComponent as backIconImg } from '../../assets/icons/backIcon.svg';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -9,9 +10,10 @@ const Header = () => {
   return (
     <Wrapper>
       <Layout>
-        <button type="button" onClick={() => navigate(-1)}>
-          뒤로가기
-        </button>
+        <BackBtnContainer onClick={() => navigate(-1)}>
+          <BackIcon />
+          <p>뒤로가기</p>
+        </BackBtnContainer>
         {role === 'STUDENT_COUNCIL' && <p>관리자용</p>}
       </Layout>
       <Title>공지사항</Title>
@@ -20,19 +22,27 @@ const Header = () => {
 };
 
 const Wrapper = styled.div`
-  background-color: pink;
   width: 100%;
   position: sticky;
   top: 0;
   z-index: 999;
+  border: 1px solid red;
 `;
 
 const Layout = styled.div`
   display: flex;
 `;
 
+const BackBtnContainer = styled.div`
+  border: 1px solid skyblue;
+  display: flex;
+  align-items: center;
+`;
+
 const Title = styled.h2`
   font-size: 24px;
 `;
+
+const BackIcon = styled(backIconImg)``;
 
 export default Header;
