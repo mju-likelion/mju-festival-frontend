abad40532373df2430e3651ae4d01904f47e2c00
zelkovaria <hyein396@gmail.com>
2024-09-18 18:36:23 +0900

    design: map img 영역 css - #66

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import mapImg from '../../assets/imgs/MapImg.png';
 
 const Map = () => {
   return (
@@ -7,19 +8,25 @@ const Map = () => {
         <p>지도</p>
         <p>각 부스 및 행사장 위치를 파악하고 즐기세요!</p>
       </TitleLayout>
+      <MapImgLayout>
+        <MapImg src={mapImg} alt="mapImg" />
+      </MapImgLayout>
+      <IconsLayout />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  padding: 6px 20px 88px 20px;
 `;
 
 const TitleLayout = styled.div`
   display: flex;
   flex-direction: column;
   gap: 9px;
-  padding: 6px 20px 56px 20px;
+  border: 1px solid red;
 
   p:nth-of-type(1) {
     font-size: 20px;
@@ -29,6 +36,20 @@ const TitleLayout = styled.div`
     font-size: 16px;
     font-weight: 400;
   }
+`;
+const MapImgLayout = styled.div`
+  padding-top: 56px;
+  border: 1px solid red;
+`;
+
+const MapImg = styled.img`
+  width: 100%;
+  height: 478px;
+`;
+
+const IconsLayout = styled.div`
+  padding-top: 48px;
+  border: 1px solid pink;
 `;
 
 export default Map;
