55811566246ed003dd785df400424e69e0e36593
zelkovaria <hyein396@gmail.com>
2024-09-18 21:20:12 +0900

    design: 상단의 텍스트 영역 추가 및 css 적용 - #69

--- src/pages/TimeTable/index.tsx
+++ src/pages/TimeTable/index.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import styled from 'styled-components';
 
 const TimeTable = () => {
-  return <div>timetable</div>;
+  return (
+    <Wrapper>
+      <TitleLayout>
+        <p>타임테이블</p>
+        <p>
+          각 시간대별 공연을 확인하고
+          <br />
+          재미있는 공연을 즐겨보세요!
+        </p>
+      </TitleLayout>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 23px 0 28px 20px;
+  gap: 9px;
+  border: 1px solid red;
+
+  p:nth-of-type(1) {
+    font-size: 20px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.text900};
+  }
+  p:nth-of-type(2) {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: normal;
+    color: ${({ theme }) => theme.colors.text900};
+  }
+`;
 export default TimeTable;
