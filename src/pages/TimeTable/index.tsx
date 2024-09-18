3a6e31f5b53ac2a6e2765fd1308b9871b27da738
zelkovaria <hyein396@gmail.com>
2024-09-19 00:11:21 +0900

    refactor: titmetableInfo 구조 변경 - #69

--- src/pages/TimeTable/index.tsx
+++ src/pages/TimeTable/index.tsx
@@ -1,30 +1,56 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import InfoText from '../../components/InfoText';
 import { PerformanceType, TimeTableInfo } from '../../types';
 
 const TimeTable = () => {
-  const october7Info: TimeTableInfo[] = [
-    {
-      type: '댄스',
-      image: '/path/to/dance.jpg',
-      teamName: '댄스 공연 input',
-    },
-    {
-      type: '밴드',
-      image: '/path/to/band.jpg',
-      teamName: '밴드 공연 input',
-    },
-    {
-      type: '힙합',
-      image: '/path/to/hiphop.jpg',
-      teamName: '힙합 공연 input',
-    },
-    {
-      type: '축하공연',
-      image: '/path/to/celebration.jpg',
-      teamName: '축하공연 input',
-    },
-  ];
+  const [selectedDate, setSelectedDate] = useState('10월 07일');
+  const timetableInfo: TimeTableInfo[] = {
+    '10월 07일': [
+      {
+        type: '댄스',
+        image: '/path/to/dance.jpg',
+        teamName: '댄스 공연 input',
+      },
+      {
+        type: '밴드',
+        image: '/path/to/band.jpg',
+        teamName: '밴드 공연 input',
+      },
+      {
+        type: '힙합',
+        image: '/path/to/hiphop.jpg',
+        teamName: '힙합 공연 input',
+      },
+      {
+        type: '축하공연',
+        image: '/path/to/celebration.jpg',
+        teamName: '축하공연 input',
+      },
+    ],
+    '10월 08일': [
+      {
+        type: '댄스',
+        image: '/path/to/dance.jpg',
+        teamName: '댄스 공연 input',
+      },
+      {
+        type: '밴드',
+        image: '/path/to/band.jpg',
+        teamName: '밴드 공연 input',
+      },
+      {
+        type: '힙합',
+        image: '/path/to/hiphop.jpg',
+        teamName: '힙합 공연 input',
+      },
+      {
+        type: '축하공연',
+        image: '/path/to/celebration.jpg',
+        teamName: '축하공연 input',
+      },
+    ],
+  };
 
   return (
     <Wrapper>
@@ -40,12 +66,30 @@ const TimeTable = () => {
         <InfoText>타임 테이블</InfoText>
       </InfoTextLayout>
       <TypeLayout>
-        {october7Info.map((item) => (
+        {timetableInfo.map((item) => (
           <TypeIcon type={item.type} key={item.teamName}>
             <p>{item.type}</p>
           </TypeIcon>
         ))}
       </TypeLayout>
+      <DateLayout>
+        <ButtonContainer>
+          <DateButton
+            onClick={() => {
+              setSelectedDate('10월 07일');
+            }}
+          >
+            <p>10월 07일</p>
+          </DateButton>
+          <DateButton
+            onClick={() => {
+              setSelectedDate('10월 08일');
+            }}
+          >
+            <p>10월 08일</p>
+          </DateButton>
+        </ButtonContainer>
+      </DateLayout>
     </Wrapper>
   );
 };
@@ -84,6 +128,7 @@ const TypeLayout = styled.div`
   display: flex;
   padding: 0 44px;
   gap: 10px;
+  border: 1px solid yellow;
 `;
 
 const TypeIcon = styled.div<{ type: PerformanceType }>`
@@ -110,5 +155,28 @@ const TypeIcon = styled.div<{ type: PerformanceType }>`
         return theme.colors.white100;
     }
   }};
+`;
+
+const DateLayout = styled.div`
+  padding: 20px 30px 22px 30px;
+  border: 1px solid green;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  border-radius: 999px;
+  background-color: #bfcbd2;
+
+  p {
+    /* width: 100%; */
+    color: #7b858e;
+    font-size: 17px;
+    font-weight: 600;
+  }
+`;
+const DateButton = styled.button`
+  width: 100%;
+  height: 38px;
+  padding: 9px 0;
 `;
 export default TimeTable;
