79f4ebd6533d72f49e97ee4e8532ddf0a2bf9fbf
zelkovaria <hyein396@gmail.com>
2024-09-21 14:45:22 +0900

    feat: footer 직책 추가 - #71

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -110,8 +110,8 @@ const Main = () => {
         </ButtonContainer>
       </FestivalInfoLayout>
       <Footer>
-        <ContentContainer>
-          <IconLayout>
+        <ContentLayout>
+          <IconContainer>
             <IconBox>
               <FooterLinkIcon />
               <p>https://www.mju.ac.kr/mjukr/index.do</p>
@@ -120,13 +120,70 @@ const Main = () => {
               <FooterHomeIcon />
               <p>https://open.kakao.com/o/sneAIT0f</p>
             </IconBox>
-          </IconLayout>
+          </IconContainer>
           <FooterMJUIcon />
-        </ContentContainer>
+        </ContentLayout>
+        <PeopleLayout>
+          <PeopleContainer>
+            <PeopleBox>
+              <p>design</p>
+              <p>김소현</p>
+            </PeopleBox>
+            <PeopleBox>
+              <p>frontend</p>
+              <p>오현의 이진혁 정혜인</p>
+            </PeopleBox>
+            <PeopleBox>
+              <p>backend</p>
+              <p>김대현</p>
+            </PeopleBox>
+          </PeopleContainer>
+          <PeopleContainer>
+            <PeopleBox>
+              <p>총학생회장</p>
+              <p>이승준</p>
+            </PeopleBox>
+            <PeopleBox>
+              <p>홍보국장</p>
+              <p>이우진</p>
+            </PeopleBox>
+            <PeopleBox>
+              <p>대외협력국장</p>
+              <p>이채영</p>
+            </PeopleBox>
+          </PeopleContainer>
+        </PeopleLayout>
       </Footer>
     </Wrapper>
   );
 };
+
+const PeopleLayout = styled.div`
+  display: flex;
+  padding-right: 36px;
+`;
+const PeopleContainer = styled.div`
+  padding-top: 4px;
+`;
+
+const PeopleBox = styled.div`
+  display: flex;
+  width: 100%;
+  width: 167px;
+  height: 16px;
+  gap: 4px;
+
+  p:nth-of-type(1) {
+    font-size: 11px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.white100};
+  }
+  p:nth-of-type(2) {
+    font-size: 11px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.white100};
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -263,13 +320,13 @@ const Footer = styled.div`
   padding: 14px 14px 16px 20px;
 `;
 
-const ContentContainer = styled.div`
+const ContentLayout = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
 
-const IconLayout = styled.div`
+const IconContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 6px;
@@ -278,6 +335,7 @@ const IconLayout = styled.div`
 const IconBox = styled.div`
   display: flex;
   align-items: center;
+  gap: 4px;
 
   p {
     font-size: 13px;
