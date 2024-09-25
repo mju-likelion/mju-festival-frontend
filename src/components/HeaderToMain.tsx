34234ac9b416035ad07aa6a1e9f8c5fd33a94c77
zelkovaria <hyein396@gmail.com>
2024-09-26 00:27:55 +0900

    refactor: header 경로 수정 - #117

---
+++ src/components/HeaderToMain.tsx
@@ -0,0 +1,63 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { ReactComponent as MenuIconSvg } from '../assets/icons/hamburger.svg';
+import { ReactComponent as BackIcon } from '../assets/icons/left_arrow.svg';
+import { useAuthStore } from '../store';
+
+const HeaderToMain = () => {
+  const navigate = useNavigate();
+  const { role } = useAuthStore();
+
+  return (
+    <Wrapper>
+      <BackButton onClick={() => navigate('/main')}>
+        <BackIcon />
+        뒤로가기
+      </BackButton>
+      <AdminMenuLayout>
+        {(role === 'STUDENT_COUNCIL' || role === 'BOOTH_MANAGER') && (
+          <Role>관리자용</Role>
+        )}
+        <MenuIcon />
+      </AdminMenuLayout>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  justify-content: space-between;
+  background-color: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(10px);
+  z-index: 999;
+  padding: 4px 20px 0 11px; // bottom: 0 - 각자 페이지 조절
+`;
+
+const BackButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1px;
+  color: ${({ theme }) => theme.colors.text500};
+  ${({ theme }) => theme.typographies.footnote};
+  cursor: pointer;
+`;
+
+const AdminMenuLayout = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  gap: 7px;
+`;
+
+const Role = styled.p`
+  color: ${({ theme }) => theme.colors.text500};
+  ${({ theme }) => theme.typographies.subhead2};
+`;
+
+const MenuIcon = styled(MenuIconSvg)`
+  cursor: pointer;
+`;
+
+export default HeaderToMain;
--- src/pages/ViewAllNotice/index.tsx
+++ src/pages/ViewAllNotice/index.tsx
@@ -5,7 +5,7 @@ import { useAuthStore, usePageStore } from '../../store';
 import useFetchNotices from '../../hooks/useFetchNotices';
 import InfoText from '../../components/InfoText';
 import TitleLayout from './Header.tsx';
-import Header from '../../components/Header.tsx';
+import HeadeToMain from '../../components/HeaderToMain.tsx';
 import { ReactComponent as LeftArrowActive } from '../../assets/icons/left_arrow_active.svg';
 import { ReactComponent as RightArrowActive } from '../../assets/icons/right_arrow_active.svg';
 import DropDown from './DropDown.tsx';
@@ -25,7 +25,7 @@ const ViewAllNotice = () => {
 
   return (
     <Wrapper>
-      <Header />
+      <HeadeToMain />
       <TitleLayout />
       <InfoTextLayout>
         <InfoText>공지사항</InfoText>
