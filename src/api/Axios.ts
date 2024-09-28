14fc019514ce02d94c4735576aca064440aa4385
zelkovaria <hyein396@gmail.com>
2024-09-29 00:07:58 +0900

    feat: Axios 인터셉터로 로그인 예외처리 및 로그아웃 구현 - #128

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import { useAuthStore } from '../store';
+import { ERRORS } from '../types/errorCode';
 
 export const Axios = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+Axios.interceptors.response.use(undefined, (res) => {
+  if (ERRORS.has(res.data.errorCode)) {
+    useAuthStore.setState({
+      token: '',
+      role: '',
+    });
+    // 메시지는 zustand로 관리해서 해당 컴포넌트에서 사용해서 띄울 예정
+  }
+  return res;
 });
 
 export const AxiosWeather = axios.create({
--- src/types/errorCode.ts
+++ src/types/errorCode.ts
@@ -3,5 +3,6 @@ export const ERROR_CODES = {
   UNAUTHORIZED: 40104,
   EXPIRED: 40105,
 } as const;
+export const ERRORS = new Set(Object.values(ERROR_CODES));
 
 export type ErrorCode = (typeof ERROR_CODES)[keyof typeof ERROR_CODES];
