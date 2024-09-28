82a46c0d74a4d3816ffa4bd493b7008c4f75d058
hyunyeee <5004ohy@naver.com>
2024-09-29 02:03:30 +0900

    fix: 404페이지 헤더 제거 - #133

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,15 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import Header from '../../components/Header';
 
 const Index = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <Header path="/main" />
-      <Wrapper>
-        <Text>404 NotFound</Text>
-        <Button type="button" onClick={() => navigate('/main')}>
-          메인페이지로 이동하기
-        </Button>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Text>404 Not Found</Text>
+      <Button type="button" onClick={() => navigate('/main')}>
+        메인페이지로 이동하기
+      </Button>
+    </Wrapper>
   );
 };
 
