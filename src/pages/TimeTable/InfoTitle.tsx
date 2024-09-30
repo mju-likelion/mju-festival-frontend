0ed8cd36bea4122f839c795be6b018463eabb8df
zelkovaria <hyein396@gmail.com>
2024-09-30 14:50:40 +0900

    design: 공연 정보타이틀 컴포넌트 구현 - #142

---
+++ src/pages/TimeTable/InfoTitle.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { ReactComponent as Dashed } from '../../assets/imgs/timetable_dashed.svg';
+
+interface InfoTitleProps {
+  children: React.ReactNode;
+}
+const InfoTitle = ({ children }: InfoTitleProps) => {
+  return (
+    <Wrapper>
+      <DashedLine />
+      <Title>{children}</Title>
+      <DashedLine />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  width: 100%;
+`;
+const Title = styled.p`
+  text-align: center;
+  ${({ theme }) => theme.typographies.body1};
+`;
+const DashedLine = styled(Dashed)``;
+
+export default InfoTitle;
--- src/pages/TimeTable/index.tsx
+++ src/pages/TimeTable/index.tsx
@@ -5,6 +5,7 @@ import InfoText from '../../components/InfoText';
 import { TimeTableDate } from '../../types';
 import UnivPerformance from './UnivPerformance.tsx';
 import ClubPerformance from './ClubPerformance.tsx';
+import InfoTitle from './InfoTitle.tsx';
 
 const TimeTable = () => {
   const [selectedDate, setSelectedDate] = useState<TimeTableDate>('10월 07일');
@@ -46,12 +47,14 @@ const TimeTable = () => {
       <ContextLayout>
         {selectedDate === '10월 07일' && (
           <UniversityContainer>
+            <InfoTitle>학교 내부 공연</InfoTitle>
             <UnivPerformance title="백마 체전 시상식" time="18:20 ~ 18:30" />
             <ClubPerformance date="10월 07일" />
           </UniversityContainer>
         )}
         {selectedDate === '10월 08일' && (
           <UniversityContainer>
+            <InfoTitle>학교 내부 공연</InfoTitle>
             <UnivPerformance title="명지대 응원단 연합" time="18:05 ~ 18:25" />
             <ClubPerformance date="10월 08일" />
             <UnivPerformance title="유끼즈" time="19:10 ~ 19:50" />
