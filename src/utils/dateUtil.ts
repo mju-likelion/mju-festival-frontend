d5753bef8c24265fca54d8f6d63d514faf6e3d3e
Kim Daehyeon <daehyeon3351@gmail.com>
2024-10-02 23:47:57 +0900

    refactor: 시간, 분까지 포매팅하도록 변경 - #159

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -2,7 +2,9 @@ export const formatDate = (date: Date): string => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
-  return `${year} . ${month} . ${day}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  return `${year}.${month}.${day}   ${hours}:${minutes}`;
 };
 
 export const getCurrentDate = (): string => {
