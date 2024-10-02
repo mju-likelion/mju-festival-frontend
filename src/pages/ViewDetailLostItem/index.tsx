73ef0659168647656271919b7fccfeefeaefad00
constantly-dev <rntqhdl10@naver.com>
2024-10-02 23:47:12 +0900

    feat: 분실물 상세 조회 페이지 잘못된 경로 막기 구현 - #155

--- src/pages/ViewDetailLostItem/index.tsx
+++ src/pages/ViewDetailLostItem/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { deleteLostItem, patchLostItemAsFound } from '../../api/lostItem';
@@ -17,7 +17,15 @@ const DetailLostItem = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const { token, role } = useAuthStore();
-  const { title, content, createdAt, imageUrl, isFounded } = location.state;
+  const { title, content, createdAt, imageUrl, isFounded } =
+    location.state || {};
+
+  useEffect(() => {
+    if (!location.state) {
+      alert('잘못된 접근입니다.');
+      navigate('/lost-items');
+    }
+  }, [location.state, navigate]);
 
   const formattedDate = useMemo(
     () => formatDate(new Date(createdAt)),
